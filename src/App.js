5ffed8499cfb5863a3a3d3c7be9ffeb373b56fe6
Tanzim Ahmed Chowdhury <kayserfoyez68@gmail.com>
2019-06-19 16:59:15 -0500

    React Router Implemented

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import React from 'react'
 import './App.css'
 import Search from './Search';
 import Main from './Main';
+import {Route} from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
-    showSearchPage: false
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <Search/>
-        ) : (
-          <Main/>
-        )}
+        <Route exact path='/' component={Main}/>
+        <Route exact path='/search' component={Search}   />
       <div className="open-search">
-         <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+         {/* <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button> */}
       </div>
       </div>
     )
