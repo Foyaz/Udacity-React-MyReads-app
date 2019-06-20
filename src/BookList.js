6c8acd10fc4149f6d5be49b7c9355fd31ad39994
Tanzim Ahmed Chowdhury <kayserfoyez68@gmail.com>
2019-06-19 19:49:42 -0500

    images fetched

--- src/BookList.js
+++ src/BookList.js
@@ -5,14 +5,61 @@ class BookList extends React.Component{
     render(){
         return(
         <div>
+            
             <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <h2 className="bookshelf-title">Currently Reading</h2>
                 <div className="bookshelf-books">
                 <ol className="books-grid">
-                    <Book/>
+                    {
+                        this.props.books
+                        .filter(book => book.shelf === 'currentlyReading')
+                            .map(book => (
+                                <li key={book.id}>
+                                    <Book
+                                     book ={book}/>
+                                </li>
+                             ))
+                    }
                </ol>
             </div>
           </div>
+
+          <div className="bookshelf">
+                <h2 className="bookshelf-title">Want to Read</h2>
+                <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {
+                        this.props.books.filter(
+                            book => book.shelf === 'wantToRead')
+                            .map(book => (
+                                <li key={book.id}>
+                                    <Book 
+                                    book ={book}/>
+                                </li>
+                             ))
+                    }
+               </ol>
+            </div>
+          </div>
+
+          <div className="bookshelf">
+                <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {
+                        this.props.books.filter(
+                            book => book.shelf === 'read')
+                            .map(book => (
+                                <li key={book.id}>
+                                    <Book
+                                     book ={book}/>
+                                </li>
+                             ))
+                    }
+               </ol>
+            </div>
+          </div>
+
         </div>
         )
     }
--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import BookList from './BookList'
+import * as BooksAPI from './BooksAPI'
+
 
 class Main extends React.Component{
+    state ={
+        books: [],
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            this.setState({books})
+        }
+        )
+    }
+
     render(){
         return(
             <div className="list-books">
@@ -10,9 +23,10 @@ class Main extends React.Component{
             </div>
             <div className="list-books-content">
               <div>
-                <BookList title=" Currently Reading"/>
-                <BookList title="Want to Read"/>
-                <BookList title="Read"/>
+                  <BookList books={this.state.books}/>
+                {/* <BookList books={this.state.books} title="Currently Reading"/>
+                <BookList books={this.state.books} title="Want to Read"/>
+                <BookList books={this.state.books} title="Read"/> */}
               </div>
             </div>
           </div>
